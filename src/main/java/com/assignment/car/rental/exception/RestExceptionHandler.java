70eeb895a5a058e6fd3740d9534bce6b941773e5
Shashi <d2s@Shashis-MacBook-Air.local>
2020-09-26 08:46:32 +0200

    basic flow

--- src/main/java/com/assignment/car/rental/exception/RestExceptionHandler.java
+++ src/main/java/com/assignment/car/rental/exception/RestExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.assignment.car.rental.exception;
 
+import javax.persistence.EntityNotFoundException;
+import javax.validation.ConstraintViolationException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import com.assignment.car.rental.rest.response.error.ErrorResponse;
+import com.assignment.car.rental.rest.response.ErrorResponse;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
@@ -13,6 +18,30 @@ public class RestExceptionHandler {
 	@ExceptionHandler(value = { RequestEntityAlreadyExists.class })
 	protected ResponseEntity<ErrorResponse> handle(RequestEntityAlreadyExists ex) {
 		return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponse(ex.getMessage()));
+
+	}
+
+	@ExceptionHandler(value = { EntityNotFoundException.class })
+	protected ResponseEntity<ErrorResponse> handle(EntityNotFoundException ex) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(ex.getMessage()));
+
+	}
+
+	@ExceptionHandler(value = { MethodArgumentNotValidException.class })
+	protected ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException ex) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ex.getMessage()));
+
+	}
+
+	@ExceptionHandler(value = { ConstraintViolationException.class })
+	protected ResponseEntity<ErrorResponse> handle(ConstraintViolationException ex) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ex.getMessage()));
+
+	}
+
+	@ExceptionHandler(value = { MethodArgumentTypeMismatchException.class })
+	protected ResponseEntity<ErrorResponse> handle(MethodArgumentTypeMismatchException ex) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ex.getMessage()));
 
 	}
 
