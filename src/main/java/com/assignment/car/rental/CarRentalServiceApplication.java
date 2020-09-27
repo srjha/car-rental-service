3f6f464e9e20d518ae7645527d5dbba604e5ddc0
Shashi <d2s@Shashis-MacBook-Air.local>
2020-09-27 20:21:51 +0200

    Audit

--- src/main/java/com/assignment/car/rental/CarRentalServiceApplication.java
+++ src/main/java/com/assignment/car/rental/CarRentalServiceApplication.java
@@ -1,15 +1,27 @@
 package com.assignment.car.rental;
 
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableJpaAuditing(dateTimeProviderRef = "zonedDateTimeProvider")
 public class CarRentalServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarRentalServiceApplication.class, args);
+	}
+
+	@Bean
+	public DateTimeProvider zonedDateTimeProvider() {
+		return () -> Optional.of(ZonedDateTime.now());
 	}
 
 }
--- src/main/java/com/assignment/car/rental/entities/AbstractEntity.java
+++ src/main/java/com/assignment/car/rental/entities/AbstractEntity.java
@@ -1,19 +1,31 @@
 package com.assignment.car.rental.entities;
 
 import java.io.Serializable;
+import java.time.ZonedDateTime;
 
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class AbstractEntity implements Serializable {
 	private static final long serialVersionUID = 2370892890655450122L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Long id;
+
+	@CreatedDate
+	protected ZonedDateTime createdAt;
+
+	@CreatedDate
+	protected ZonedDateTime lastUpdatedAt;
 
 	public Long getId() {
 		return id;
