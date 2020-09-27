c00f03938212c62a5acf1f89731224b5878b2424
Shashi <d2s@Shashis-MacBook-Air.local>
2020-09-27 13:04:43 +0200

    total cost

--- src/main/java/com/assignment/car/rental/entities/RentalOrder.java
+++ src/main/java/com/assignment/car/rental/entities/RentalOrder.java
@@ -1,8 +1,14 @@
 package com.assignment.car.rental.entities;
 
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 
 @Entity
 public class RentalOrder extends AbstractEntity {
@@ -15,5 +21,38 @@ public class RentalOrder extends AbstractEntity {
 	@OneToOne
 	@JoinColumn(name = "customer_id", referencedColumnName = "id")
 	private Customer customer;
+
+	private BigDecimal totalCost;
+
+	private ZonedDateTime pickupTime;
+
+	private ZonedDateTime dropoffTime;
+
+	public ZonedDateTime getPickupTime() {
+		return pickupTime;
+	}
+
+	public void setPickupTime(ZonedDateTime pickupTime) {
+		this.pickupTime = pickupTime;
+	}
+
+	public ZonedDateTime getDropoffTime() {
+		return dropoffTime;
+	}
+
+	public void setDropoffTime(ZonedDateTime dropoffTime) {
+		this.dropoffTime = dropoffTime;
+	}
+
+	public BigDecimal getTotalCost() {
+		return totalCost;
+	}
+
+	@PrePersist
+	@PreUpdate
+	public void calcTotalCost() {
+		totalCost = availibility.getPerHourRate()
+				.multiply(BigDecimal.valueOf(Duration.between(dropoffTime, pickupTime).toHours()));
+	}
 
 }
