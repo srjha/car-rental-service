af284fcb2b85f6a702a34abfa70a53dab156b6fc
Shashi <d2s@Shashis-MacBook-Air.local>
2020-09-27 12:36:16 +0200

    rental order

--- src/main/java/com/assignment/car/rental/entities/RentalOrder.java
+++ src/main/java/com/assignment/car/rental/entities/RentalOrder.java
@@ -1,12 +1,19 @@
 package com.assignment.car.rental.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
 public class RentalOrder extends AbstractEntity {
 	private static final long serialVersionUID = 7476546003712765703L;
 
+	@OneToOne
+	@JoinColumn(name = "availibility_id", referencedColumnName = "id")
 	private Availibility availibility;
+
+	@OneToOne
+	@JoinColumn(name = "customer_id", referencedColumnName = "id")
 	private Customer customer;
 
 }
